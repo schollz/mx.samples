31c5b3d5a6356796c7e71d4ca7885c55cefcea09
Zack Scholl <zack.scholl@gmail.com>
2021-01-29 14:11:38 -0800

    formatting

--- superkeys.lua
+++ superkeys.lua
@@ -8,25 +8,25 @@ superkeys=include("superkeys/lib/superkeys")
 
 engine.name="Superkeys"
 skeys=nil
-instrument_names = {}
-instrument_current = 1
+instrument_names={}
+instrument_current=1
 
 function init()
   skeys=superkeys:new()
-  instrument_names = skeys:list_instruments()
+  instrument_names=skeys:list_instruments()
 
   for _,dev in midi.devices do
-  if dev.port > 0 then 
-  m = midi.connect(dev.port)
-  m.event = function(data) 
-    tab.print(data) 
-    if data[1]==144 then 
-      skeys:on({name=instrument_names[instrument_current],midi=data[2],velocity=data[3]})
-    elseif data[1]==128 then 
-      skeys:off({name=instrument_names[instrument_current],midi=data[2]})
+    if dev.port>0 then
+      m=midi.connect(dev.port)
+      m.event=function(data)
+        tab.print(data)
+        if data[1]==144 then
+          skeys:on({name=instrument_names[instrument_current],midi=data[2],velocity=data[3]})
+        elseif data[1]==128 then
+          skeys:off({name=instrument_names[instrument_current],midi=data[2]})
+        end
+      end
     end
-  end
-end
   end
 
   clock.run(redraw_clock) -- start the grid redraw clock
@@ -36,17 +36,17 @@ function enc(k,d)
 end
 
 function key(k,z)
-  if k>=2 and z==1 then 
-    local d = 1 
-    if k == 2 then 
-      d = -1
+  if k>=2 and z==1 then
+    local d=1
+    if k==2 then
+      d=-1
     end
-      instrument_current = instrument_current + d
-    if instrument_current < 1 then 
-      instrument_current = #instrument_names
+    instrument_current=instrument_current+d
+    if instrument_current<1 then
+      instrument_current=#instrument_names
     end
-    if instrument_current > #instrument_names then 
-      instrument_current = 1
+    if instrument_current>#instrument_names then
+      instrument_current=1
     end
   end
 end
@@ -60,7 +60,7 @@ end
 
 function redraw()
   screen.clear()
-  
+
   screen.level(15)
   screen.move(10,10)
   screen.text(instrument_names[instrument_current])
