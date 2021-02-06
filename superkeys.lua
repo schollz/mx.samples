c049a02dda6631f587eb1e308fee809d9ffdef6b
Zack Scholl <zack.scholl@gmail.com>
2021-02-06 15:23:08 -0800

    add downloading

--- superkeys.lua
+++ superkeys.lua
@@ -31,6 +31,9 @@ function init()
     if dev.port~=nil then
       m=midi.connect(dev.port)
       m.event=function(data)
+        if available_instruments[instrument_current].active ~= true then 
+          do return end
+        end
         tab.print(data)
         if data[1]==144 then
           skeys:on({name=available_instruments[instrument_current].id,midi=data[2],velocity=data[3]})
@@ -55,10 +58,16 @@ function update_uilist()
     if string.find(files_for,".wav") then 
       downloaded = true
     end
-    local s = a.name
+    local s = a.name    
     available_instruments[i].downloaded = downloaded
+    available_instruments[i].active = (downloaded and i==instrument_current)
     if not downloaded then
       s = s.." - get? ("..a.size.."MB)"
+    end
+    if available_instruments[i].active then 
+      s = '> '..s..' <'
+    else
+      s = '  '..s
     end
     table.insert(items,s)
   end
@@ -81,8 +90,39 @@ function enc(k,d)
 end
 
 function key(k,z)
-  print(uilist.index)
-  instrument_current=uilist.index
+  if z == 1 then 
+    local i = uilist.index 
+    if available_instruments[i].downloaded then
+      instrument_current = i 
+    elseif download_available > 0 then 
+      if k==2 then 
+        download_available = 0 
+      elseif k==3 then 
+        -- download!
+        downloading =true 
+        clock.run(function()
+          download(available_instruments[download_available].id)
+          update_uilist()
+          download_available = 0
+          downloading = false
+      end)
+      end
+    else
+      download_available = i
+    end
+  end
+end
+
+
+function download(id) 
+  local url = "https://github.com/schollz/superkeys/releases/download/samples/"..id..".zip"
+  local download_file = _path.code.."superkeys/samples/"..id.."/download.zip"
+  cmd = "curl -o "..download_file.." "..url
+  print(cmd)
+  os.execute(cmd)
+  cmd = "unzip "..download_file.." -d ".._path.code.."superkeys/samples/"..id.."/"
+  print(cmd)
+  os.execute(cmd)
 end
 
 function redraw_clock() -- our grid redraw clock
@@ -95,10 +135,11 @@ end
 function redraw()
   screen.clear()
   if downloading then 
-    msg=UI.Message.new({"downloading","X","please wait..."})
+    msg=UI.Message.new({"downloading",available_instruments[download_available].name,"please wait..."})
     msg:redraw()
   elseif download_available > 0 then 
-    msg=UI.Message.new({"are you sure","you want to","download X (mb)?","k2 = no, k3 = yes"})
+    local s = available_instruments[download_available].name..'('..available_instruments[download_available].size..')'
+    msg=UI.Message.new({"are you sure","you want to","download "..s.."?","k2 = no, k3 = yes"})
     msg:redraw()
   else
     uilist:redraw()
